da2ff8747e13124e419e3df3185568d11ac8c399
Chadi El Masri <chadimsr@gmail.com>
2025-05-15 15:25:23 +0200

    Add more cache metrics

--- pkg/data/cached_data.go
+++ pkg/data/cached_data.go
@@ -31,30 +31,88 @@ var (
 	)
 )
 
+func registerCacheSizeMetric[K comparable, V any](cacheType string, cache *ttlcache.Cache[K, V]) {
+	promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Namespace: "idfm",
+		Name:      "cache_size",
+		Help:      "Size of the cache",
+		ConstLabels: prometheus.Labels{
+			"type": cacheType,
+		},
+	}, func() float64 {
+		return float64(cache.Len())
+	})
+}
+
+func registerCacheHitMetric[K comparable, V any](cacheType string, cache *ttlcache.Cache[K, V]) {
+	promauto.NewCounterFunc(prometheus.CounterOpts{
+		Namespace: "idfm",
+		Name:      "cache_hits",
+		Help:      "Cache hits",
+		ConstLabels: prometheus.Labels{
+			"type": cacheType,
+		},
+	}, func() float64 {
+		return float64(cache.Metrics().Hits)
+	})
+}
+
+func registerCacheMissMetric[K comparable, V any](cacheType string, cache *ttlcache.Cache[K, V]) {
+	promauto.NewCounterFunc(prometheus.CounterOpts{
+		Namespace: "idfm",
+		Name:      "cache_misses",
+		Help:      "Cache misses",
+		ConstLabels: prometheus.Labels{
+			"type": cacheType,
+		},
+	}, func() float64 {
+		return float64(cache.Metrics().Misses)
+	})
+}
+
+func registerCacheInsertionsMetric[K comparable, V any](cacheType string, cache *ttlcache.Cache[K, V]) {
+	promauto.NewCounterFunc(prometheus.CounterOpts{
+		Namespace: "idfm",
+		Name:      "cache_insertions",
+		Help:      "Cache insertions",
+		ConstLabels: prometheus.Labels{
+			"type": cacheType,
+		},
+	}, func() float64 {
+		return float64(cache.Metrics().Insertions)
+	})
+}
+
+func registerCacheEvictionsMetric[K comparable, V any](cacheType string, cache *ttlcache.Cache[K, V]) {
+	promauto.NewCounterFunc(prometheus.CounterOpts{
+		Namespace: "idfm",
+		Name:      "cache_evictions",
+		Help:      "Cache evictions",
+		ConstLabels: prometheus.Labels{
+			"type": cacheType,
+		},
+	}, func() float64 {
+		return float64(cache.Metrics().Evictions)
+	})
+}
+
 func InitCache() {
 	go TypeAndNumberToLineNameCache.Start()
 	go StopIdForDirectionCache.Start()
 
 	// Prometheus metrics
-	promauto.NewGaugeFunc(prometheus.GaugeOpts{
-		Namespace: "idfm",
-		Name:      "cache_size",
-		Help:      "Size of the cache",
-		ConstLabels: prometheus.Labels{
-			"type": "lines",
-		},
-	}, func() float64 {
-		return float64(TypeAndNumberToLineNameCache.Len())
-	})
+	registerCacheSizeMetric("stops", StopIdForDirectionCache)
+	registerCacheSizeMetric("lines", TypeAndNumberToLineNameCache)
 
-	promauto.NewGaugeFunc(prometheus.GaugeOpts{
-		Namespace: "idfm",
-		Name:      "cache_size",
-		Help:      "Size of the cache",
-		ConstLabels: prometheus.Labels{
-			"type": "stops",
-		},
-	}, func() float64 {
-		return float64(StopIdForDirectionCache.Len())
-	})
+	registerCacheHitMetric("stops", StopIdForDirectionCache)
+	registerCacheHitMetric("lines", TypeAndNumberToLineNameCache)
+
+	registerCacheMissMetric("stops", StopIdForDirectionCache)
+	registerCacheMissMetric("lines", TypeAndNumberToLineNameCache)
+
+	registerCacheInsertionsMetric("stops", StopIdForDirectionCache)
+	registerCacheInsertionsMetric("lines", TypeAndNumberToLineNameCache)
+
+	registerCacheEvictionsMetric("stops", StopIdForDirectionCache)
+	registerCacheEvictionsMetric("lines", TypeAndNumberToLineNameCache)
 }
